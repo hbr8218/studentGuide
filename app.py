622cf7aef05bd0f3731cbd9641518367e3f85a4f
Hassan Bari Raza <hbr8218@gmail.com>
2020-05-26 00:23:16 +0530

    testing on staging

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config.from_object(os.environ.get('APP_SETTINGS'))
 
 @app.route('/')
 def index():
-    return "This is webapp for students"
+    return "This is webapp for students,Deployed with {}".format(os.environ.get('APP_SETTINGS'))
 
 if __name__ == '__main__':
     app.run()

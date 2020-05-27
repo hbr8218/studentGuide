9fb756259515047245c89c24bdbfa4b121403cd2
Hassan Bari Raza <hbr8218@gmail.com>
2020-05-28 04:25:56 +0530

    pushing to studentguide-stage

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return "This is webapp for students,Deployed with {}".format(os.environ.get('APP_SETTINGS'))
+    return "This is webapp for students,Deployed with {} and db_uri is {}".format(os.environ.get('APP_SETTINGS'),os.environ.get('DATABASE_URI'))
 
 if __name__ == '__main__':
     app.run()
